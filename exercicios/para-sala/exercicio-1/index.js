b745a2c3d6550b4f988b405bf87c9a959700c9cb
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-29 23:06:32 -0300

    adjust exercises

--- exercicios/para-sala/exercicio-1/index.js
+++ exercicios/para-sala/exercicio-1/index.js
@@ -1,15 +1,17 @@
 const motorista0 = new Driver('Maria', 13);
 const motorista1 = new Driver('Babi', 38);
 const motorista2 = new Driver('Jose', 23);
-// console.log('motorista 1: ', motorista1);
-// console.log('motorista 2: ', motorista2);
+
+console.log('motorista 1: ', motorista1);
+console.log('motorista 2: ', motorista2);
 
 const passageira1 = new Passenger('Lais', 17, 1111);
 const passageira2 = new Passenger('Marta', 34, 2222);
 const passageira3 = new Passenger('Dani', 53, 3333);
-// console.log('passageira 1: ', passageira1);
-// console.log('passageira 2: ', passageira2);
-// console.log('passageira 3: ', passageira3);
+
+console.log('passageira 1: ', passageira1);
+console.log('passageira 2: ', passageira2);
+console.log('passageira 3: ', passageira3);
 
 passageira1.requestDrive(motorista1, 12, 1111);
 passageira1.requestDrive(motorista2, 35, 1111);
--- exercicios/para-sala/exercicio-2/index.js
+++ exercicios/para-sala/exercicio-2/index.js
@@ -8,11 +8,11 @@ const passageira1 = new Passenger('Luara', 27, 111);
 const passageira2 = new Passenger('Erica', 15, 222);
 const passageira3 = new Passenger('Claudia', 25, 333);
 
-// console.log(Driver.drivers);
-// console.log(Passenger.passengers);
+console.log(Driver.drivers);
+console.log(Passenger.passengers);
 
-// Driver.numberOfDrivers(Driver.drivers);
-// Passenger.numberOfPassengers(Passenger.passengers);
+Driver.numberOfDrivers();
+Passenger.numberOfPassengers();
 
-Driver.ageAverage(Driver.drivers);
-Passenger.ageAverage(Passenger.passengers);
+Driver.ageAverage();
+Passenger.ageAverage();
